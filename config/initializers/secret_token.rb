662b3ae1567d24b182473ca037a3724afe07ac48
David Van Der Beek <earlynovrock@gmail.com>
2013-09-23 10:32:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkcapital::Application.config.secret_token = '0b13294e235fba4baa294f1cf8c79b0df7999e08ee7bdb5f3c1fd064af2d614793d1d39146beb81783a5e0bdcc35443929dbb2dccf75bd8d4db7495009d0951c'
