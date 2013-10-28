161ba8634913387e98525c4aeec9063ca8556b91
David Van Der Beek <earlynovrock@gmail.com>
2013-10-28 15:30:41 -0500

    add vendor assets to precompile array

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Linkcapital::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += %w(respond.min.js html5shiv.js)
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
