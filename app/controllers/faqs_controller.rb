50d0ea25f772059aeeb104e7d18519adfbe01dc3
David Van Der Beek <earlynovrock@gmail.com>
2013-10-30 15:17:16 -0500

    fix image sizes

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,6 +1,4 @@
 class FaqsController < ApplicationController
-  # GET /faqs
-  # GET /faqs.json
   def index
     client = UserVoice::Client.new(ENV['USERVOICE_API_SUBDOMAIN'], ENV['USERVOICE_API_KEY'], ENV['USERVOICE_API_SECRET'], :callback => 'http://linkcapital.herokuapp.com/')
     @faqs = client.get("/api/v1/articles.json")
