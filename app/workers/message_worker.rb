c210950fa6b4e8a96ccab447474fafec7bbea090
David Van Der Beek <earlynovrock@gmail.com>
2013-11-01 10:51:07 -0500

    change apply now heading

--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -2,17 +2,16 @@ class MessageWorker
   include SuckerPunch::Job
 
   def perform(message)
-    ActiveRecord::Base.connection_pool.with_connection do
-      client = UserVoice::Client.new(ENV['USERVOICE_API_SUBDOMAIN'], ENV['USERVOICE_API_KEY'], ENV['USERVOICE_API_SECRET'], :callback => ENV['USERVOICE_API_CALLBACK'])
-      @sdk_question = client.post("/api/v1/tickets.json", {
-        :email => message.email,
-        :name => message.name,
-        :ticket => {
-          :subject => 'Contact Us Submission',
-          :message => (message.content + (message.phone.blank? ? "" : (" Phone: " + message.phone)))
-        }
-      })['ticket']
-      # PrivatePub.publish_to("/messages/new", "alert('Support ticket received!');")
-    end
+    sleep 5
+    client = UserVoice::Client.new(ENV['USERVOICE_API_SUBDOMAIN'], ENV['USERVOICE_API_KEY'], ENV['USERVOICE_API_SECRET'], :callback => ENV['USERVOICE_API_CALLBACK'])
+    @sdk_question = client.post("/api/v1/tickets.json", {
+      :email => message.email,
+      :name => message.name,
+      :ticket => {
+        :subject => 'Contact Us Submission',
+        :message => (message.content + (message.phone.blank? ? "" : (" Phone: " + message.phone)))
+      }
+    })['ticket']
+    # PrivatePub.publish_to("/messages/new", "alert('Support ticket received!');")
   end
 end
