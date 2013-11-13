29ad61a16e42a02ef8bdcf44e5510e0bd4d55f5f
David Van Der Beek <earlynovrock@gmail.com>
2013-11-13 10:30:10 -0600

    remove sleep call in message worker

--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -2,7 +2,6 @@ class MessageWorker
   include SuckerPunch::Job
 
   def perform(message)
-    sleep 5
     client = UserVoice::Client.new(ENV['USERVOICE_API_SUBDOMAIN'], ENV['USERVOICE_API_KEY'], ENV['USERVOICE_API_SECRET'], :callback => ENV['USERVOICE_API_CALLBACK'])
     @sdk_question = client.post("/api/v1/tickets.json", {
       :email => message.email,
