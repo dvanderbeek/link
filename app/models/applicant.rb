9581881666ab39f9e4caed0e4e549567d983e16f
David Van Der Beek <earlynovrock@gmail.com>
2013-12-12 10:04:59 -0600

    add referred_by to attr_accessible

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,5 +1,5 @@
 class Applicant < ActiveRecord::Base
-  attr_accessible :balance, :email, :name, :rate, :school
+  attr_accessible :balance, :email, :name, :rate, :school, :referred_by
   validates_presence_of :balance, :email, :name, :rate, :school
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
 end
