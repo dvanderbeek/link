bdde519c848a63c90a920808951c8b321c90f9e7
David Van Der Beek <earlynovrock@gmail.com>
2013-12-12 10:26:29 -0600

    Update applicant worker

--- app/workers/applicant_worker.rb
+++ app/workers/applicant_worker.rb
@@ -1,8 +1,16 @@
 class ApplicantWorker
   include SuckerPunch::Job
+  include ActionView::Helpers::NumberHelper
 
   def perform(applicant)
-    message = "Name: #{applicant.name}, Email: #{applicant.email}, School: #{applicant.school}, Balance: #{applicant.balance.to_s}, Rate: #{applicant.rate.to_s}, Referred By: #{applicant.referred_by.nil? ? 'N/A' : applicant.referred_by}"
+    message = <<-EOF
+      Name: #{applicant.name} \r\n
+      Email: #{applicant.email} \r\n
+      School: #{applicant.school} \r\n
+      Balance: #{number_to_currency applicant.balance} \r\n
+      Rate: #{number_to_percentage applicant.rate} \r\n
+      Referred By: #{applicant.referred_by.nil? ? 'N/A' : applicant.referred_by}
+    EOF
 
     client = UserVoice::Client.new(ENV['USERVOICE_API_SUBDOMAIN'], ENV['USERVOICE_API_KEY'], ENV['USERVOICE_API_SECRET'], :callback => ENV['USERVOICE_API_CALLBACK'])
     @sdk_question = client.post("/api/v1/tickets.json", {
