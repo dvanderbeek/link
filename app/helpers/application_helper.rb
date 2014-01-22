cd5e45267c5dfade1e51578e2cf01791438c7ea0
David Van Der Beek <earlynovrock@gmail.com>
2014-01-22 10:37:20 -0600

    style updates and set up table for savings calculator results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     fields = render("home/loan_fields")
     link_to(name, '#', class: "add_fields", data: {fields: fields.gsub("\n", "")})
   end
+
+  def fixed_terms
+    %w[3 5 7 10 15 20 30]
+  end
+
+  def variable_terms
+    %w[5 10]
+  end
 end
