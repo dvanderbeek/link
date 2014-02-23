c19a4419d9c586dca2468dbfd8de9add095a25ff
David Van Der Beek <earlynovrock@gmail.com>
2014-02-23 15:38:13 -0600

    update page routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Linkcapital::Application.routes.draw do
   get :apply, to: 'applicants#new', as: :new_applicant
   get :apply, to: 'applicants#new', as: :apply
 
-  %w[students investors organizations privacy_policy about_us terms_of_use].each do |path|
+  %w[investors privacy_policy about_us terms_of_use].each do |path|
     get path, to: "home##{path}", as: path
   end
+  get "borrowers", to: "home#students", as: :students
+  get "employers", to: "home#organizations", as: :organizations
+  
   get :contact, to: 'messages#new', as: :contact
   resources :messages, only: [:new, :create]
   resources :employer_contacts, only: [:create]
